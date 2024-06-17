ebd58fd931432ab57e8190a6b8364f47c781089f
luca-jt <random.lulu@die-genossen.de>
2024-06-17 23:28:39 +0200

    different renderer storage

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,15 +1,23 @@
+use std::collections::HashMap;
+
 pub trait Renderer {
     fn render(&mut self);
 }
 
 pub struct RenderingSystem {
-    renderers: Vec<Box<dyn Renderer>>,
+    renderers: HashMap<String, Box<dyn Renderer>>,
+    // just do hardcoded renderer lists for every type?
 }
 
 impl RenderingSystem {
+    /// add a renderer to the system
+    pub fn add_renderer(&mut self, name: String, renderer: impl Renderer) {
+        self.renderers.insert(name, Box::new(renderer));
+    }
+
     /// start the rendering for all renderers
     pub fn render(&mut self) {
-        for renderer in self.renderers.iter_mut() {
+        for renderer in self.renderers.values_mut() {
             renderer.render();
         }
     }
