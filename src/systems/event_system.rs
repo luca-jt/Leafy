e17935dbc6c856e7182d04af20af2ba92bc3b69e
luca-jt <random.lulu@die-genossen.de>
2024-07-06 02:10:08 +0200

    few new events

--- src/systems/event_system.rs
+++ src/systems/event_system.rs
@@ -1,5 +1,7 @@
+use sdl2::controller::{Axis, Button};
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::{MouseButton, MouseWheelDirection};
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -62,7 +64,78 @@ impl EventSystem {
                 } => {
                     if let WindowEvent::Resized(width, height) = win_event {
                         polled_events.push(FLEvent::WindowResize(width, height))
+                    } else if win_event == WindowEvent::FocusGained {
+                        polled_events.push(FLEvent::WindowGainedFocus);
+                    } else if win_event == WindowEvent::FocusLost {
+                        polled_events.push(FLEvent::WindowLostFocus);
                     }
+                }
+                Event::ControllerButtonDown {
+                    timestamp: _,
+                    which: _,
+                    button,
+                } => {
+                    polled_events.push(FLEvent::ControllerButton(button));
+                }
+                Event::ControllerAxisMotion {
+                    timestamp: _,
+                    which: _,
+                    axis,
+                    value,
+                } => {
+                    polled_events.push(FLEvent::ControllerAxis(axis, value));
+                }
+                Event::MouseButtonDown {
+                    timestamp: _,
+                    window_id: _,
+                    which: _,
+                    mouse_btn,
+                    clicks: _,
+                    x,
+                    y,
+                } => {
+                    polled_events.push(FLEvent::MouseClick {
+                        button: mouse_btn,
+                        at_x: x as u32,
+                        at_y: y as u32,
+                    });
+                }
+                Event::MouseMotion {
+                    timestamp: _,
+                    window_id: _,
+                    which: _,
+                    mousestate: _,
+                    x,
+                    y,
+                    xrel,
+                    yrel,
+                } => {
+                    polled_events.push(FLEvent::MouseMove {
+                        new_x: x as u32,
+                        new_y: y as u32,
+                        rel_x: xrel as u32,
+                        rel_y: yrel as u32,
+                    });
+                }
+                Event::MouseWheel {
+                    timestamp: _,
+                    window_id: _,
+                    which: _,
+                    x: _,
+                    y: _,
+                    direction,
+                    precise_x,
+                    precise_y,
+                    mouse_x,
+                    mouse_y,
+                } => {
+                    polled_events.push(FLEvent::MouseScroll {
+                        direction,
+                        scroll_x: precise_x,
+                        scroll_y: precise_y,
+                        at_x: mouse_x as u32,
+                        at_y: mouse_y as u32,
+                    });
                 }
                 _ => {}
             }
@@ -84,12 +157,29 @@ pub trait EventObserver {
 /// all of the supported event types for callbacks
 pub enum FLEvent {
     KeyPress(Keycode),
-    MouseMove(f32, f32),
-    MouseScroll(f32),
-    MouseClick,
+    MouseMove {
+        new_x: u32,
+        new_y: u32,
+        rel_x: u32,
+        rel_y: u32,
+    },
+    MouseScroll {
+        direction: MouseWheelDirection,
+        scroll_x: f32,
+        scroll_y: f32,
+        at_x: u32,
+        at_y: u32,
+    },
+    MouseClick {
+        button: MouseButton,
+        at_x: u32,
+        at_y: u32,
+    },
+    ControllerAxis(Axis, i16),
+    ControllerButton(Button),
     WindowResize(i32, i32),
-    WindowLooseFocus,
-    WindowGainFocus,
+    WindowLostFocus,
+    WindowGainedFocus,
 }
 
 /*
