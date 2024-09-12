61c901d3be13cb4eee1aa4db9657549a59f7a62a
luca-jt <random.lulu@die-genossen.de>
2024-09-12 16:25:58 +0200

    project structure change

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod ecs;
 pub mod engine;
+pub mod engine_builder;
 pub mod rendering;
 pub mod systems;
 pub mod utils;
--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -2,8 +2,8 @@ use crate::ecs::component::Acceleration;
 use crate::glm;
 
 pub const WIN_TITLE: &str = "Falling Leaf";
-pub const MIN_WIN_WIDTH: u32 = 1200;
-pub const MIN_WIN_HEIGHT: u32 = 675;
+pub const MIN_WIN_WIDTH: u32 = 800;
+pub const MIN_WIN_HEIGHT: u32 = 450;
 pub const INV_WIN_RATIO: f32 = 9.0 / 16.0;
 
 pub const MAX_TEXTURE_COUNT: usize = 32;
